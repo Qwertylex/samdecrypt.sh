3c0c14ae3bf9ce208a5e6cfa47698cdd914af481
Alex <Qwertylex@gmail.com>
2021-04-03 14:40:06 -0400

    Update samdecrypt.sh

--- samdecrypt.sh
+++ samdecrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#find your key in fumagician (look for base64)
-#key=8337838b2345aa7662cd902a9731ef52f4506275b57ca7cd9c36b565bf993cd1 #key for EMT02B6Q
+#find your key in fumagician or check known_keys.txt
+#look for base64 or use this oneliner: strings fumagician | grep -E '^[A-Za-z0-9+/]{42}[AEIMQUYcgkosw048]=$' | base64 -d | xxd -p -c 32
 key=8c7ef2b7836d88ca0abd8f2c91d4eeba4f4d71762df745ee341d2bacbc0f86c0 #key for 2B2QEXM7
 openssl enc -aes-256-ecb -d -in "$1" -nopad -K "$key" > "${1/.enc/.bintemp}"
 magic=5f696369616e4d41475f402a212e3826
